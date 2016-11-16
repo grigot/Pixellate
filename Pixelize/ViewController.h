93486e127bd34dccbcae4f14d4014f6aeb88397d
Guy Rigot <guy.rigot@gmail.com>
2016-11-16 21:17:45 +0100

    Initial Commit

---
+++ Pixelize/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pixelize
+//
+//  Created by Guy Rigot on 16-11-16.
+//  Copyright © 2016 Guy Rigot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Pixelize/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Pixelize
+//
+//  Created by Guy Rigot on 16-11-16.
+//  Copyright © 2016 Guy Rigot. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
